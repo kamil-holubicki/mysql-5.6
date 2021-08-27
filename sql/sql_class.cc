e1b7da38e7f75bd5a56148568144eb2e4c6cd3a0
Chi Li <chili@fb.com>
2021-08-27 12:13:09 -0700

    add cmd_line flag to dscp_on_socket
    Summary: Without the flag, the configuration can not persistent. Having it in cnf file will crash during startup.
    
    Reviewed By: yizhang82
    
    Differential Revision: D30611389
    
    fbshipit-source-id: 5daf6c411ff

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -4407,11 +4407,6 @@ bool THD::set_dscp_on_socket() {
     return false;
   }
 
-  if (dscp_val == 0) {
-    // Default unset value
-    return true;
-  }
-
   NET *net = get_protocol_classic()->get_net();
 
   int tos = dscp_val << 2;
